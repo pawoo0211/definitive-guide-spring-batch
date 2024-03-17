ff5ab2d4ce5a7f809285169747dce27cd82d5420
pawoo0211 <pawoo0211@naver.com>
2024-03-17 17:24:24 +0900

    Add Validator

--- src/main/java/com/example/SpringBatchTutorial/HelloWorld.java
+++ src/main/java/com/example/SpringBatchTutorial/HelloWorld.java
@@ -4,10 +4,17 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class HelloWorld {
@@ -18,8 +25,9 @@ public class HelloWorld {
 
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("basicJob-1")                // 잡 파라미터
+        return jobBuilderFactory.get("basicJob-2")                // 잡 파라미터
                 .start(step1())
+                .validator(validator())
                 .build();
     }
 
@@ -30,5 +38,35 @@ public class HelloWorld {
                     System.out.println("HelloWorld!!");
                     return RepeatStatus.FINISHED;
                 }).build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['fileName']}") String fileName) {
+    return ((stepContribution, chunkContext) -> {
+        System.out.println(
+                String.format("Hello, %s!", name));
+        System.out.println(
+                String.format("fileName=%s", fileName));
+        return RepeatStatus.FINISHED;
+        });
+    }
+
+    @Bean
+    public CompositeJobParametersValidator validator() {
+        // Instantiate CompositeJobParametersValidator
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+        // Instantiate DefaultJobParametersValidator
+        DefaultJobParametersValidator defaultValidator = new DefaultJobParametersValidator();
+        defaultValidator.setRequiredKeys(new String[] {"fileName"});
+        defaultValidator.setOptionalKeys(new String[] {"name"});
+        defaultValidator.afterPropertiesSet();
+
+        // Inject into CompositeJobParametersValidator
+        validator.setValidators(Arrays.asList(new ParameterValidator(), defaultValidator));
+        return validator;
     }
 }
